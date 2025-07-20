f9aa429f95eb51964e3e21238650288f314fad2f
topu11 <touhidulislam256@gmail.com>
2025-07-21 01:34:55 +0600

    touhidul add

--- react-fragments/src/App.jsx
+++ react-fragments/src/App.jsx
@@ -1,17 +1,14 @@
 function App() {
   const items=["A","B","C","D","E","F"];
-  const strlower=(item)=>{
-   return item.toLowerCase()
-  }
+  const strlower=(item)=>item.toLowerCase()
+  
   return (
     <>
       <div className="container">
         <div>Healty Foods</div>
         <ul className="list-group">
         {
-          items.map((item,index)=>
-              <li key={index} className="list-group-item">{strlower(item)}</li>
-          )
+          items.map((item,index)=><li className="litst-group-item" key={index}>{strlower(item)}</li>)
         }
         </ul>
       </div>
