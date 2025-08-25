eb1f59ff9b957fa67c758ab6054d397b7f8932bd
topu11 <touhidulislam256@gmail.com>
2025-08-25 07:14:18 +0600

    rr

--- react-fragments/src/App.jsx
+++ react-fragments/src/App.jsx
@@ -1,11 +1,13 @@
 function App() {
   const items=["A","B","C","D","E","F"];
+   //const items=[];
   const strlower=(item)=>item.toLowerCase()
   
   return (
     <>
       <div className="container">
         <div>Healty Foods</div>
+        <h2>{items.length==0 && `I am hungry`}</h2>
         <ul className="list-group">
         {
           items.map((item,index)=><li className="list-group-item" key={index}>{strlower(item)}</li>)
